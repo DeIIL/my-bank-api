325cb3f7685b88f34a2456b6bb87a51ea73b2c26
DeIIL <gudellcastillo@gmail.com>
2023-02-20 18:55:31 -0300

    inplemented graphql

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
 import winston from "winston";
+
+import { buildSchema } from "graphql";
+
 import accountsRouter from "./routes/account.routes.js";
+import accountService from "./services/account.service.js";
+
 import { promises as fs } from "fs";
 
 const { readFile, writeFile } = fs;
@@ -20,10 +26,57 @@ global.logger = winston.createLogger({
   format: combine(label({ label: "my-bank-api" }), timestamp(), myFormat),
 });
 
+const schema = buildSchema(`
+  type Account {
+    id: Int
+    name: String
+    balance: Float
+  }
+  input AccountInput {
+    id: Int
+    name: String
+    balance: Float
+  }
+  type Query {
+    getAccount: [Account]
+    getAccountById(id: Int): Account
+  }
+  type Mutation {
+    createAccount(account: AccountInput): Account
+    deleteAccount(id: Int): Boolean
+    updateAccount(account: AccountInput): Account
+  }
+`);
+
+const root = {
+  getAccount: () => accountService.getAccount(),
+  getAccountById(args) {
+    return accountService.getAccountById(args.id);
+  },
+  createAccount({ account }) {
+    return accountService.createAccount(account);
+  },
+  deleteAccount(args) {
+    accountService.deleteAccountById(args.id);
+  },
+  updateAccount({ account }) {
+    return accountService.updateAccount(account);
+  },
+};
+
 const app = express();
 app.use(express.json());
 
 app.use("/account", accountsRouter);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(3333, async () => {
   try {
